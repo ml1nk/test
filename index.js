48dc8fe03e8c617a6915f872ab3eaeb2d809c803
Marcel Link <marcel.link@t-online.de>
2016-07-11 22:15:02 +0200

    + requirejs integration

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
-/*
-
-
-var promise = new Promise(function(resolve, reject) {
-  // do a thing, possibly async, then…
-
-  if (true) {
-    resolve("Stuff worked!");
-  }
-  else {
-    reject();
-  }
+var requirejs = require('requirejs');
+requirejs.config({
+    nodeRequire: require
 });
 
-promise.then(function(){
-  console.log("test");
-});*/
-var program = require('commander');
+requirejs(['server/cmd.js', 'express', 'http', 'socket.io'], function (cmd, express, http, io) {
+  var config = cmd(__dirname, process.argv);
 
-program
-  .version('0.0.1')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-  .parse(process.argv);
+  var app = express();
+  var http = http.Server(app);
+  var io = require('socket.io')(http);
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+  io.on('connection', function(socket){
+    console.log('a user connected');
+  });
+
+  http.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
+
+  app.get('/', function(req, res){
+    res.sendFile(__dirname + '/client/index.html');
+  });
+  app.use(express.static(__dirname + '/client/'+(config.dev ? "dev" : "min")));
+  app.use("/lib/",express.static(__dirname + '/client/lib'));
+
+
+
 });
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-app.use('/static', express.static(__dirname + '/public'));
---
+++ server/cmd.js
@@ -0,0 +1,12 @@
+define(['commander'], function (program) {
+  return function(dirname, argv) {
+    program
+      .version(require(dirname + '/package').version)
+      .option('-d, --dev', 'developer mode')
+      .parse(argv);
+
+    return {
+      dev : program.dev ? true : false
+    }
+  }
+});
