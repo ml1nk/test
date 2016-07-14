38ab419d2584010018a4018a66b2ba5ec061fd7f
Marcel Link <marcel.link@t-online.de>
2016-07-15 00:45:16 +0200

    + integrate mariasql

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var config = require("./src/cmd.js")(__dirname, process.argv);
 process.env.NODE_ENV = config.dev ? 'development' : 'production';
 
+var Client = require('mariasql');
+var c = new Client({
+  host: config.db.host,
+  user: config.db.user,
+  password: config.db.pas,
+  db: config.db.data
+});
+
+
 var browserify = require('browserify-middleware');
 var express = require("express");
 var app = express();
@@ -18,3 +27,25 @@ http.listen(3000, function() {
 
 app.use(express.static(__dirname + '/public/'));
 app.get('/index.js', browserify('./src/client/startup.js'));
+
+
+process.on('SIGINT', function () {
+  console.log("shutdown - http")
+  http.close(function () {
+    console.log("shutdown - mariasql")
+    if(!c.connected) {
+      console.log("shutdown - mariasql - already down")
+      process.exit(0);
+    } else {
+      c.on("end",function(){
+        console.log("shutdown - mariasql - end")
+        process.exit(0);
+      });
+      c.on("close",function(){
+        console.log("shutdown - mariasql - close")
+        process.exit(0);
+      });
+      c.end();
+    }
+  });
+});
